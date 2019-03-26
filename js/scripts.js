1fb863c493c2533afd0b1f2b92515fa5a0fd48b4
alex martinez and evan hadfield <alex@>
2019-03-26 15:03:55 -0700

    add flashcard framework

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function() {
+  $(".card1-clickable").click(function() {
+    $("#javascript").show();
+    $("#java-def").toggle();
+  });
+});
+$(function() {
+  $(".card2-clickable").click(function() {
+    $("#operators").show();
+    $("#op-def").toggle();
+  });
+});
